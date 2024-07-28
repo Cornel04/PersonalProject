4f38694ed104cebb4466ab3a7586ed9f3bdfbb79
Cornel04 <104451188+Cornel04@users.noreply.github.com>
2024-07-28 15:40:52 +0300

    k-means finisihed

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ from test_folder.k_means import plot_results
 
 if __name__ == "__main__":
     # Create a sample dataset
-    X = np.array([[1.0, 2.0], [1.5, 1.8], [5.0, 8.0], [8.0, 8.0], 
-                  [1.0, 0.6], [9.0, 11.0], [8.0, 2.0], [10.0, 2.0], [9.0, 3.0]])
+    num_points = 1000
+    X = np.random.rand(num_points, 2)
 
     # Set number of clusters
     k = 3
-
+                
     # Run K-Means
     centroids, labels = kmeans(X, k)
 
